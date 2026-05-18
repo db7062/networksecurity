8e7cb0936e39d0d2065adc60eb8c5cce8b4d2c54
Debjeet Banerjee <debjeet.mailme@gmail.com>
2026-05-18 23:18:03 +0530

    Data Transformation Completed

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
+
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
-from networksecurity.entity.config_entity import TrainingPipelineConfig
-from networksecurity.components.data_validation import DataValidation,DataValidationConfig
-from networksecurity.entity.artifact_entity import DataValidationArtifact
-import os
 
+from networksecurity.entity.config_entity import (
+    DataIngestionConfig,
+    DataValidationConfig,
+    DataTransformationConfig
+)
+
+from networksecurity.entity.config_entity import TrainingPipelineConfig
+import os
 import sys
+
 if __name__=='__main__':
   try:
-    import os
+   
 
     # print("Current Working Directory:", os.getcwd())
     # print("Schema Exists:", os.path.exists("data_schema/schema.yml"))
@@ -27,6 +34,11 @@ if __name__=='__main__':
     data_validation_artifact=data_validation.initiate_data_validation()
     logging.info(" data validation completed")
     print(data_validation_artifact)
-    
+    data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+    logging.info("Data transformation started")
+    data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+    data_transformation_artifact=data_transformation.initiate_data_transformation()
+    print(data_transformation)
+    logging.info("Data transformation ended")
   except Exception as e:
     raise NetworkSecurityException(e,sys)
